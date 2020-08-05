dacde7d8dd1ccd93f6c923790feefc89c8bbbf11
Ankit Prasad <ankit28prasad9@gmail.com>
2020-08-05 19:10:47 +0530

    initial

---
+++ CSS/style.php
@@ -0,0 +1,30 @@
+<style type="text/css">
+
+*{margin: 0; padding: 0; box-sizing: border-box; font-family: 'Recursive', sans-serif;}
+@media only screen and (max-width:986px){
+#navbarSupportedContent{
+    background-color: rgba(31, 5, 59, 0.822) !important;
+}
+}
+.nav_style{
+    background-color: rgba(10, 2, 19, 0.89) !important;
+    height: 60px !important;
+}
+.nav_style a{
+    color: whitesmoke !important;
+    text-decoration: none;
+    height: 100% !important;
+       
+}
+.nav_style a:hover,
+.nav_style a:active{
+    color: white !important;
+}
+.nav_style .nav-item:hover{
+    background-color: rgba(164, 151, 179, 0.89);
+    height: 60px;
+}
+
+
+
+</style>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="author" content="Ankit Prasad">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="rgba(22, 21, 63, 0.952)">
+    <title>COVID-19</title>
+    <link rel="icon" href="imes.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php include 'CSS/style.php'; ?>
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
+    <link href="https://fonts.googleapis.com/css2?family=Recursive:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Code|Ubuntu:400,600|Dancing+Script:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark nav_style p-0">
+        <a class="navbar-brand pl-4 py-3" href="#">COVID-19 <i class="fa fa-atom pl-2"></i></a>
+        <div class="pr-2">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon dark-bg"></span>
+        </button>
+        </div>
+        <div class="collapse navbar-collapse justify-content-around" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link px-4" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-4" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-4" href="#">Symptoms</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-4" href="#">Prevention</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-4" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" src="scripts/jquery.backtotop.js"></script>
+    <script type="text/javascript" src="scripts/jquery.navbar-fix-top.js"></script>
+    <script type="text/javascript" src="scripts/jquery.sticky-social-bar.js"></script>
+</body>
+
+</html>
